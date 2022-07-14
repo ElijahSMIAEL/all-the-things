3e4fe998fc121f1709cf54641b1a9895bca716ab
Jesse Meirink <jesse@meirink.com>
2022-07-14 12:57:36 -0500

    test push

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -5,6 +5,7 @@ const Landing = () => {
     <>
       {/* All the <Link> components should live here */}
       <h1>All-The-Things</h1>
+      <h2>Test!!!!!!</h2>
       <Link to="/the-manliest-things">Ben's Things</Link><br/>
       <Link to="/the-functional-things">Shahzad's Things</Link><br/>
       <Link to="/the-well-styled-things">David's Things</Link><br/>
